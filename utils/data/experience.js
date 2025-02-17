eb4453e79cee8af3b5d8fd1c245d264727c1a05f
riteshaf <ritesh.raj@affinate.com>
2025-02-17 15:43:13 +0530

    chores:changed experience

--- utils/data/experience.js
+++ utils/data/experience.js
@@ -2,19 +2,13 @@ export const experiences = [
   {
     id: 1,
     title: 'Software Engineer I',
-    company: "Teton Private Ltd.",
-    duration: "(Jan 2022 - Present)"
+    company: "Matrix Solutions.",
+    duration: "(Sept 2023 - Present)"
   },
   {
     id: 2,
-    title: "FullStack Developer",
-    company: "Fiverr (freelance)",
-    duration: "(Jun 2021 - Jan 2022)"
-  },
-  {
-    id: 3,
     title: "Self Employed",
     company: "Code and build something in everyday.",
-    duration: "(Jan 2018 - Present)"
+    duration: "(Jan 2018 - Mar 2023)"
   }
 ]
