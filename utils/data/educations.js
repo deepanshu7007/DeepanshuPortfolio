441fc3f5acd7513e70fa819360fa66558b4f1b5a
riteshaf <ritesh.raj@affinate.com>
2025-02-17 15:47:44 +0530

    chores:changed Edutcation

--- utils/data/educations.js
+++ utils/data/educations.js
@@ -1,20 +1,14 @@
 export const educations = [
   {
     id: 1,
-    title: "Bachelor Degree",
-    duration: "2020 - Present",
-    institution: "National University of Bangladesh",
+    title: "Bachelor of Computer Application",
+    duration: "2018 - 2021",
+    institution: "Chandigarh University",
   },
   {
     id: 2,
-    title: "Higher Secondary Certificate",
-    duration: "2018 - 2020",
-    institution: "Noakhali Islamia Kamil Madrasah",
-  },
-  {
-    id: 3,
-    title: "Secondary School Certificate",
-    duration: "2008 - 2018",
-    institution: "Baitus Saif Islamia Madrasah",
+    title: "Masters of Computer Application",
+    duration: "2021 - 2023",
+    institution: "University of Jammu",
   }
 ]
