3172dee02ff025719ee5f69743f3c10557f25f2b
deepanshu7007 <deepanshu@signitysolutions.com>
2025-12-26 13:45:42 +0530

    fix:disabled telegram code

--- utils/data/contactsData.js
+++ utils/data/contactsData.js
@@ -1,11 +1,11 @@
 export const contactsData = {
-    email: 'abusaid7388@gmail.com',
-    phone: '+8801608797655',
-    address: 'Middle Badda, Dhaka, Bangladesh - 1212 ',
-    github: 'https://github.com/said7388',
+    email: 'deepanshu7007@gmail.com',
+    phone: '+918899725205',
+    address: '1152,sector 67,Mohali,punjab',
+    github: 'https://github.com/deepanshu7007',
     facebook: 'https://www.facebook.com/abusaid.riyaz/',
     linkedIn: 'https://www.linkedin.com/in/abu-said-bd/',
     twitter: 'https://twitter.com/said7388',
     stackOverflow: 'https://stackoverflow.com/users/16840768/abu-said',
-    devUsername: "said7388"
+    devUsername: "deepanshu7007"
 }
